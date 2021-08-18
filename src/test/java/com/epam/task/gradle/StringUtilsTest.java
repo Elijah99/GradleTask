1392f958758e2ff8beb1a336d092e82f5b9d015a
Ilya Romanovskiy <iromanovsky05@gmail.com>
2021-08-18 12:59:48 +0300

    added tests for all cases

--- src/test/java/com/epam/task/gradle/StringUtilsTest.java
+++ src/test/java/com/epam/task/gradle/StringUtilsTest.java
@@ -1,14 +1,35 @@
 package com.epam.task.gradle;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringUtilsTest {
 
     @Test
-    void testShouldReturnTrueWhenNumberPositive() {
+    void testIsPositiveNumberShouldReturnTrueWhenNumberPositive() {
         assertTrue(StringUtils.isPositiveNumber("11"));
+    }
 
+    @Test
+    void testIsPositiveNumberShouldReturnFalseWhenNumbersNegative() {
+        assertFalse(StringUtils.isPositiveNumber("-12"));
+    }
+
+    @Test
+    void testIsPositiveNumberShouldReturnTrueWhenNumberNull() {
+        assertTrue(StringUtils.isPositiveNumber("0"));
+    }
+
+    @Test
+    void testIsPositiveNumberShouldFalseTrueWhenInputString() {
+        assertFalse(StringUtils.isPositiveNumber("some string"));
+    }
+
+    @Test
+    void testIsPositiveNumberShouldFalseTrueWhenInputEmpty() {
+        assertFalse(StringUtils.isPositiveNumber(""));
     }
 
 }
